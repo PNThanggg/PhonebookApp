5945a2d3ba87cd7f352cb3f52c2a5cc1392f5e94
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-02-26 18:22:30 +0700

    update

---
+++ app/src/main/java/com/app/phonebook/presentation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.app.phonebook.presentation
+
+import android.view.LayoutInflater
+import com.app.phonebook.base.view.BaseActivity
+import com.app.phonebook.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initListener() {
+        TODO("Not yet implemented")
+    }
+
+    override fun inflateViewBinding(inflater: LayoutInflater): ActivityMainBinding {
+        return ActivityMainBinding.inflate(inflater)
+    }
+
+}
