67c8aad98b32097862fd09850071367f1167c2e8
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-02-26 18:58:48 +0700

    splash screen

--- app/src/main/java/com/app/phonebook/presentation/SplashActivity.kt
+++ app/src/main/java/com/app/phonebook/presentation/SplashActivity.kt
@@ -1,14 +1,25 @@
 package com.app.phonebook.presentation
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
+import androidx.lifecycle.lifecycleScope
 import com.app.phonebook.base.view.BaseActivity
 import com.app.phonebook.databinding.ActivitySplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
     override fun initView() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(1000)
 
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun initData() {
