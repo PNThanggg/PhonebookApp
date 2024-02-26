c273b10ada902a30220cd7b5de65fbc8004a0355
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-02-26 18:11:51 +0700

    Create BaseActivity.kt

---
+++ app/src/main/java/com/app/phonebook/base/view/BaseActivity.kt
@@ -0,0 +1,70 @@
+package com.app.phonebook.base.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.ViewBinding
+import com.app.phonebook.base.extension.finishWithSlide
+import com.app.phonebook.base.extension.handleBackPressed
+import com.app.phonebook.base.utils.Constant
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+    //region variable
+    companion object {
+        const val TAG = Constant.TAG
+        const val TIME_DELAY_CLICK = 200L
+    }
+
+    lateinit var binding: VB
+    private var isAvailableClick = true
+    //endregion
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflateViewBinding(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        initData()
+        initListener()
+        handleBackPressed {
+            onBack()
+        }
+    }
+
+    open fun onBack() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            onBackPressedDispatcher.onBackPressed()
+        } else {
+            finishWithSlide()
+        }
+    }
+
+    abstract fun initView()
+    abstract fun initData()
+    abstract fun initListener()
+
+    /**override it and inflate your view binding, demo in MainActivity*/
+    abstract fun inflateViewBinding(inflater: LayoutInflater): VB
+
+    private fun delayClick() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            isAvailableClick = false
+            delay(TIME_DELAY_CLICK)
+            isAvailableClick = true
+        }
+    }
+
+    fun View.clickSafety(action: () -> Unit) {
+        this.setOnClickListener {
+            if (isAvailableClick) {
+                action()
+                delayClick()
+            }
+        }
+    }
+}
