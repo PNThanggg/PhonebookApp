24f362e915d25f726fb4c49285a6f306ae46c068
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-03-14 08:32:25 +0700

    bottom sheet

---
+++ app/src/main/java/com/app/phonebook/base/view/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,58 @@
+package com.app.phonebook.base.view
+
+import android.graphics.drawable.LayerDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import com.app.phonebook.R
+import com.app.phonebook.base.extension.applyColorFilter
+import com.app.phonebook.base.extension.baseConfig
+import com.app.phonebook.base.extension.getProperBackgroundColor
+import com.app.phonebook.base.extension.getProperTextColor
+import com.app.phonebook.base.extension.isBlackAndWhiteTheme
+import com.app.phonebook.base.extension.setTextOrBeGone
+import com.app.phonebook.databinding.DialogBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = DialogBottomSheetBinding.inflate(inflater, container, false)
+        val context = requireContext()
+        val config = context.baseConfig
+
+        if (requireContext().isBlackAndWhiteTheme()) {
+            view.root.background = ResourcesCompat.getDrawable(
+                context.resources, R.drawable.bottom_sheet_bg_black, context.theme
+            )
+        } else if (!config.isUsingSystemTheme) {
+            view.root.background = ResourcesCompat.getDrawable(
+                context.resources, R.drawable.bottom_sheet_bg, context.theme
+            ).apply {
+                (this as LayerDrawable).findDrawableByLayerId(R.id.bottom_sheet_background)
+                    .applyColorFilter(context.getProperBackgroundColor())
+            }
+        }
+        return view.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val title = arguments?.getInt(BOTTOM_SHEET_TITLE).takeIf { it != 0 }
+        DialogBottomSheetBinding.bind(view).apply {
+            bottomSheetTitle.setTextColor(view.context.getProperTextColor())
+            bottomSheetTitle.setTextOrBeGone(title)
+            setupContentView(bottomSheetContentHolder)
+        }
+    }
+
+    abstract fun setupContentView(parent: ViewGroup)
+
+    companion object {
+        const val BOTTOM_SHEET_TITLE = "title_string"
+    }
+}
