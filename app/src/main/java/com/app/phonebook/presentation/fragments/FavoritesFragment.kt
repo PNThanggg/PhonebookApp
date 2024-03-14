7eac812c010f70c69ad0b9c26f7b6bee910d9793
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-03-15 03:09:38 +0700

    create:: more fragment

---
+++ app/src/main/java/com/app/phonebook/presentation/fragments/FavoritesFragment.kt
@@ -0,0 +1,250 @@
+package com.app.phonebook.presentation.fragments
+
+import android.content.Context
+import android.util.AttributeSet
+import com.app.phonebook.R
+import com.app.phonebook.adapter.ContactsAdapter
+import com.app.phonebook.base.extension.areSystemAnimationsEnabled
+import com.app.phonebook.base.extension.baseConfig
+import com.app.phonebook.base.extension.beGone
+import com.app.phonebook.base.extension.beVisible
+import com.app.phonebook.base.extension.beVisibleIf
+import com.app.phonebook.base.extension.config
+import com.app.phonebook.base.extension.getColorStateList
+import com.app.phonebook.base.extension.getContrastColor
+import com.app.phonebook.base.extension.getMyContactsCursor
+import com.app.phonebook.base.extension.hasPermission
+import com.app.phonebook.base.extension.normalizeString
+import com.app.phonebook.base.extension.telephonyManager
+import com.app.phonebook.base.helpers.Converters
+import com.app.phonebook.base.interfaces.RefreshItemsListener
+import com.app.phonebook.base.utils.PERMISSION_READ_CONTACTS
+import com.app.phonebook.base.utils.SMT_PRIVATE
+import com.app.phonebook.base.utils.VIEW_TYPE_GRID
+import com.app.phonebook.base.view.BaseActivity
+import com.app.phonebook.base.view.BaseRecyclerViewAdapter
+import com.app.phonebook.base.view.BaseViewPagerFragment
+import com.app.phonebook.data.models.Contact
+import com.app.phonebook.databinding.FragmentFavoritesBinding
+import com.app.phonebook.databinding.FragmentLettersLayoutBinding
+import com.app.phonebook.helpers.ContactsHelper
+import com.app.phonebook.helpers.MyContactsContentProvider
+import com.app.phonebook.presentation.view.FastScrollItemIndicator
+import com.app.phonebook.presentation.view.MyGridLayoutManager
+import com.app.phonebook.presentation.view.MyLinearLayoutManager
+import com.google.gson.Gson
+import java.util.Locale
+
+class FavoritesFragment(context: Context, attributeSet: AttributeSet) :
+    BaseViewPagerFragment<BaseViewPagerFragment.LettersInnerBinding>(context, attributeSet),
+    RefreshItemsListener {
+    private lateinit var binding: FragmentLettersLayoutBinding
+    private var allContacts = ArrayList<Contact>()
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        binding =
+            FragmentLettersLayoutBinding.bind(FragmentFavoritesBinding.bind(this).favoritesFragment)
+        innerBinding = LettersInnerBinding(binding)
+    }
+
+    override fun setupFragment() {
+        val placeholderResId = if (context.hasPermission(PERMISSION_READ_CONTACTS)) {
+            R.string.no_contacts_found
+        } else {
+            R.string.could_not_access_contacts
+        }
+
+        binding.fragmentPlaceholder.text = context.getString(placeholderResId)
+        binding.fragmentPlaceholder2.beGone()
+    }
+
+    override fun setupColors(textColor: Int, primaryColor: Int, properPrimaryColor: Int) {
+        binding.apply {
+            fragmentPlaceholder.setTextColor(textColor)
+            (fragmentList.adapter as? BaseRecyclerViewAdapter)?.updateTextColor(textColor)
+
+            letterFastscroller.textColor = textColor.getColorStateList()
+            letterFastscroller.pressedTextColor = properPrimaryColor
+            letterFastscrollerThumb.setupWithFastScroller(letterFastscroller)
+            letterFastscrollerThumb.textColor = properPrimaryColor.getContrastColor()
+            letterFastscrollerThumb.thumbColor = properPrimaryColor.getColorStateList()
+        }
+    }
+
+    override fun refreshItems(callback: (() -> Unit)?) {
+        ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
+            allContacts = contacts
+
+            if (SMT_PRIVATE !in context.baseConfig.ignoredContactSources) {
+                val privateCursor = context?.getMyContactsCursor(true, true)
+                val privateContacts = MyContactsContentProvider.getContacts(privateCursor).map {
+                    it.copy(starred = 1)
+                }
+                if (privateContacts.isNotEmpty()) {
+                    allContacts.addAll(privateContacts)
+                    allContacts.sort()
+                }
+            }
+            val favorites = contacts.filter { it.starred == 1 } as ArrayList<Contact>
+
+            allContacts = if (activity!!.config.isCustomOrderSelected) {
+                sortByCustomOrder(favorites)
+            } else {
+                favorites
+            }
+
+            activity?.runOnUiThread {
+                gotContacts(allContacts)
+                callback?.invoke()
+            }
+        }
+    }
+
+    private fun gotContacts(contacts: ArrayList<Contact>) {
+        setupLetterFastScroller(contacts)
+        binding.apply {
+            if (contacts.isEmpty()) {
+                fragmentPlaceholder.beVisible()
+                fragmentList.beGone()
+            } else {
+                fragmentPlaceholder.beGone()
+                fragmentList.beVisible()
+
+                updateListAdapter()
+            }
+        }
+    }
+
+    private fun updateListAdapter() {
+        val viewType = context.config.viewType
+        setViewType(viewType)
+
+        val currAdapter = binding.fragmentList.adapter as ContactsAdapter?
+        if (currAdapter == null) {
+            ContactsAdapter(
+                activity = activity as BaseActivity,
+                contacts = allContacts,
+                recyclerView = binding.fragmentList,
+                refreshItemsListener = this,
+                viewType = viewType,
+                showDeleteButton = false,
+                enableDrag = true,
+            ) {
+//                if (context.config.showCallConfirmation) {
+//                    CallConfirmationDialog(
+//                        activity as BaseActivity, (it as Contact).getNameToDisplay()
+//                    ) {
+//                        activity?.apply {
+//                            initiateCall(it) { launchCallIntent(it) }
+//                        }
+//                    }
+//                } else {
+//                    activity?.apply {
+//                        initiateCall(it as Contact) { launchCallIntent(it) }
+//                    }
+//                }
+            }.apply {
+                binding.fragmentList.adapter = this
+
+                onDragEndListener = {
+                    val adapter = binding.fragmentList.adapter
+                    if (adapter is ContactsAdapter) {
+                        val items = adapter.contacts
+                        saveCustomOrderToPrefs(items)
+                        setupLetterFastScroller(items)
+                    }
+                }
+
+                onSpanCountListener = { newSpanCount ->
+                    context.config.contactsGridColumnCount = newSpanCount
+                }
+            }
+
+            if (context.areSystemAnimationsEnabled) {
+                binding.fragmentList.scheduleLayoutAnimation()
+            }
+        } else {
+            currAdapter.viewType = viewType
+            currAdapter.updateItems(allContacts)
+        }
+    }
+
+    fun columnCountChanged() {
+        (binding.fragmentList.layoutManager as MyGridLayoutManager).spanCount =
+            context!!.config.contactsGridColumnCount
+        binding.fragmentList.adapter?.apply {
+            notifyItemRangeChanged(0, allContacts.size)
+        }
+    }
+
+    private fun sortByCustomOrder(favorites: List<Contact>): ArrayList<Contact> {
+        val favoritesOrder = activity!!.config.favoritesContactsOrder
+
+        if (favoritesOrder.isEmpty()) {
+            return ArrayList(favorites)
+        }
+
+        val orderList = Converters().jsonToStringList(favoritesOrder)
+        val map = orderList.withIndex().associate { it.value to it.index }
+        val sorted = favorites.sortedBy { map[it.contactId.toString()] }
+
+        return ArrayList(sorted)
+    }
+
+    private fun saveCustomOrderToPrefs(items: List<Contact>) {
+        activity?.apply {
+            val orderIds = items.map { it.contactId }
+            val orderGsonString = Gson().toJson(orderIds)
+            config.favoritesContactsOrder = orderGsonString
+        }
+    }
+
+    private fun setupLetterFastScroller(contacts: List<Contact>) {
+        binding.letterFastscroller.setupWithRecyclerView(binding.fragmentList, { position ->
+            try {
+                val name = contacts[position].getNameToDisplay()
+                val character = if (name.isNotEmpty()) name.substring(0, 1) else ""
+                FastScrollItemIndicator.Text(
+                    character.uppercase(Locale.getDefault()).normalizeString()
+                )
+            } catch (e: Exception) {
+                FastScrollItemIndicator.Text("")
+            }
+        })
+    }
+
+    override fun onSearchClosed() {
+        binding.fragmentPlaceholder.beVisibleIf(allContacts.isEmpty())
+        (binding.fragmentList.adapter as? ContactsAdapter)?.updateItems(allContacts)
+        setupLetterFastScroller(allContacts)
+    }
+
+    override fun onSearchQueryChanged(context: Context, text: String) {
+        val contacts = allContacts.filter {
+            it.name.contains(text, true) || it.doesContainPhoneNumber(
+                text = text,
+                telephonyManager = context.telephonyManager
+            )
+        }.sortedByDescending {
+            it.name.startsWith(text, true)
+        }.toMutableList() as ArrayList<Contact>
+
+        binding.fragmentPlaceholder.beVisibleIf(contacts.isEmpty())
+        (binding.fragmentList.adapter as? ContactsAdapter)?.updateItems(contacts, text)
+        setupLetterFastScroller(contacts)
+    }
+
+    private fun setViewType(viewType: Int) {
+        val spanCount = context.config.contactsGridColumnCount
+
+        val layoutManager = if (viewType == VIEW_TYPE_GRID) {
+            binding.letterFastscroller.beGone()
+            MyGridLayoutManager(context, spanCount)
+        } else {
+            binding.letterFastscroller.beVisible()
+            MyLinearLayoutManager(context)
+        }
+        binding.fragmentList.layoutManager = layoutManager
+    }
+}
---
+++ app/src/main/java/com/app/phonebook/presentation/fragments/RecentFragment.kt
@@ -0,0 +1,238 @@
+package com.app.phonebook.presentation.fragments
+
+import android.content.Context
+import android.util.AttributeSet
+import com.app.phonebook.R
+import com.app.phonebook.adapter.RecentCallsAdapter
+import com.app.phonebook.base.extension.areSystemAnimationsEnabled
+import com.app.phonebook.base.extension.baseConfig
+import com.app.phonebook.base.extension.beGone
+import com.app.phonebook.base.extension.beGoneIf
+import com.app.phonebook.base.extension.beVisible
+import com.app.phonebook.base.extension.beVisibleIf
+import com.app.phonebook.base.extension.config
+import com.app.phonebook.base.extension.getMyContactsCursor
+import com.app.phonebook.base.extension.hasPermission
+import com.app.phonebook.base.extension.telephonyManager
+import com.app.phonebook.base.extension.underlineText
+import com.app.phonebook.base.interfaces.RefreshItemsListener
+import com.app.phonebook.base.utils.MIN_RECENTS_THRESHOLD
+import com.app.phonebook.base.utils.PERMISSION_READ_CALL_LOG
+import com.app.phonebook.base.utils.SMT_PRIVATE
+import com.app.phonebook.base.view.BaseActivity
+import com.app.phonebook.base.view.BaseViewPagerFragment
+import com.app.phonebook.data.models.Contact
+import com.app.phonebook.data.models.RecentCall
+import com.app.phonebook.databinding.FragmentRecentBinding
+import com.app.phonebook.helpers.ContactsHelper
+import com.app.phonebook.helpers.MyContactsContentProvider
+import com.app.phonebook.helpers.RecentHelper
+import com.app.phonebook.presentation.view.MyRecyclerView
+
+class RecentFragment(context: Context, attributeSet: AttributeSet) :
+    BaseViewPagerFragment<BaseViewPagerFragment.RecentInnerBinding>(context, attributeSet),
+    RefreshItemsListener {
+    private lateinit var binding: FragmentRecentBinding
+    private var allRecentCalls = listOf<RecentCall>()
+    private var recentAdapter: RecentCallsAdapter? = null
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        binding = FragmentRecentBinding.bind(this)
+        innerBinding = RecentInnerBinding(binding)
+    }
+
+    override fun setupFragment() {
+        val placeholderResId = if (context.hasPermission(PERMISSION_READ_CALL_LOG)) {
+            R.string.no_previous_calls
+        } else {
+            R.string.could_not_access_the_call_history
+        }
+
+        binding.recentsPlaceholder.text = context.getString(placeholderResId)
+        binding.recentsPlaceholder2.apply {
+            underlineText()
+            setOnClickListener {
+                requestCallLogPermission()
+            }
+        }
+    }
+
+    override fun setupColors(textColor: Int, primaryColor: Int, properPrimaryColor: Int) {
+        binding.recentsPlaceholder.setTextColor(textColor)
+        binding.recentsPlaceholder2.setTextColor(properPrimaryColor)
+
+        recentAdapter?.apply {
+            initDrawables()
+            updateTextColor(textColor)
+        }
+    }
+
+    override fun refreshItems(callback: (() -> Unit)?) {
+        val privateCursor = context?.getMyContactsCursor(false, true)
+        val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
+        val querySize = allRecentCalls.size.coerceAtLeast(MIN_RECENTS_THRESHOLD)
+        RecentHelper(context).getRecentCalls(groupSubsequentCalls, querySize) { recent ->
+            ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
+                val privateContacts = MyContactsContentProvider.getContacts(privateCursor)
+
+                allRecentCalls = recent.setNamesIfEmpty(
+                    context = context, contacts = contacts, privateContacts = privateContacts
+                ).hidePrivateContacts(
+                    privateContacts, SMT_PRIVATE in context.baseConfig.ignoredContactSources
+                )
+
+                activity?.runOnUiThread {
+                    gotRecents(allRecentCalls)
+                }
+            }
+        }
+    }
+
+    private fun gotRecents(recents: List<RecentCall>) {
+        if (recents.isEmpty()) {
+            binding.apply {
+                recentsPlaceholder.beVisible()
+                recentsPlaceholder2.beGoneIf(context.hasPermission(PERMISSION_READ_CALL_LOG))
+                recentsList.beGone()
+            }
+        } else {
+            binding.apply {
+                recentsPlaceholder.beGone()
+                recentsPlaceholder2.beGone()
+                recentsList.beVisible()
+            }
+
+            if (binding.recentsList.adapter == null) {
+                recentAdapter = RecentCallsAdapter(
+                    activity as BaseActivity,
+                    recents.toMutableList(),
+                    binding.recentsList,
+                    this,
+                    true
+                ) {
+                    val recentCall = it as RecentCall
+                    if (context.config.showCallConfirmation) {
+//                        CallConfirmationDialog(activity as BaseActivity, recentCall.name) {
+//                            activity?.launchCallIntent(recentCall.phoneNumber)
+//                        }
+                    } else {
+                        activity?.launchCallIntent(recentCall.phoneNumber)
+                    }
+                }
+
+                binding.recentsList.adapter = recentAdapter
+
+                if (context.areSystemAnimationsEnabled) {
+                    binding.recentsList.scheduleLayoutAnimation()
+                }
+
+                binding.recentsList.endlessScrollListener =
+                    object : MyRecyclerView.EndlessScrollListener {
+                        override fun updateTop() {}
+
+                        override fun updateBottom() {
+                            getMoreRecentCalls()
+                        }
+                    }
+
+            } else {
+                recentAdapter?.updateItems(recents)
+            }
+        }
+    }
+
+    private fun getMoreRecentCalls() {
+        val privateCursor = context?.getMyContactsCursor(false, true)
+        val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
+        val querySize = allRecentCalls.size.plus(MIN_RECENTS_THRESHOLD)
+        RecentHelper(context).getRecentCalls(groupSubsequentCalls, querySize) { recents ->
+            ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
+                val privateContacts = MyContactsContentProvider.getContacts(privateCursor)
+
+                allRecentCalls = recents.setNamesIfEmpty(
+                    context = context, contacts = contacts, privateContacts = privateContacts
+                ).hidePrivateContacts(
+                    privateContacts, SMT_PRIVATE in context.baseConfig.ignoredContactSources
+                )
+
+                activity?.runOnUiThread {
+                    gotRecents(allRecentCalls)
+                }
+            }
+        }
+    }
+
+    private fun requestCallLogPermission() {
+        activity?.handlePermission(PERMISSION_READ_CALL_LOG) {
+            if (it) {
+                binding.recentsPlaceholder.text = context.getString(R.string.no_previous_calls)
+                binding.recentsPlaceholder2.beGone()
+
+                val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
+                RecentHelper(context).getRecentCalls(groupSubsequentCalls) { recents ->
+                    activity?.runOnUiThread {
+                        gotRecents(recents)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onSearchClosed() {
+        binding.recentsPlaceholder.beVisibleIf(allRecentCalls.isEmpty())
+        recentAdapter?.updateItems(allRecentCalls)
+    }
+
+    override fun onSearchQueryChanged(context: Context, text: String) {
+        val recentCalls = allRecentCalls.filter {
+            it.name.contains(text, true) || it.doesContainPhoneNumber(
+                text = text, telephonyManager = context.telephonyManager
+            )
+        }.sortedByDescending {
+            it.name.startsWith(text, true)
+        }.toMutableList() as ArrayList<RecentCall>
+
+        binding.recentsPlaceholder.beVisibleIf(recentCalls.isEmpty())
+        recentAdapter?.updateItems(recentCalls, text)
+    }
+}
+
+// hide private contacts from recent calls
+private fun List<RecentCall>.hidePrivateContacts(
+    privateContacts: List<Contact>, shouldHide: Boolean
+): List<RecentCall> {
+    return if (shouldHide) {
+        filterNot { recent ->
+            val privateNumbers = privateContacts.flatMap { it.phoneNumbers }.map { it.value }
+            recent.phoneNumber in privateNumbers
+        }
+    } else {
+        this
+    }
+}
+
+private fun List<RecentCall>.setNamesIfEmpty(
+    context: Context, contacts: List<Contact>, privateContacts: List<Contact>
+): ArrayList<RecentCall> {
+    val contactsWithNumbers = contacts.filter { it.phoneNumbers.isNotEmpty() }
+    return map { recent ->
+        if (recent.phoneNumber == recent.name) {
+            val privateContact = privateContacts.firstOrNull {
+                it.doesContainPhoneNumber(
+                    recent.phoneNumber, telephonyManager = context.telephonyManager
+                )
+            }
+            val contact =
+                contactsWithNumbers.firstOrNull { it.phoneNumbers.first().normalizedNumber == recent.phoneNumber }
+
+            when {
+                privateContact != null -> recent.copy(name = privateContact.getNameToDisplay())
+                contact != null -> recent.copy(name = contact.getNameToDisplay())
+                else -> recent
+            }
+        } else {
+            recent
+        }
+    } as ArrayList
+}
