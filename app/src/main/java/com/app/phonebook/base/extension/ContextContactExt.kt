0647d086f9b23d7c82defe032f66b44c0fc59fad
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-03-15 00:35:55 +0700

    update ContextContactExt.kt

--- app/src/main/java/com/app/phonebook/base/extension/ContextContactExt.kt
+++ app/src/main/java/com/app/phonebook/base/extension/ContextContactExt.kt
@@ -1,6 +1,10 @@
 package com.app.phonebook.base.extension
 
 import android.content.Context
+import android.database.Cursor
+import android.provider.ContactsContract
+import android.util.Log
+import com.app.phonebook.base.utils.APP_NAME
 import com.app.phonebook.base.utils.DEFAULT_MIMETYPE
 import com.app.phonebook.base.utils.PERMISSION_READ_CONTACTS
 import com.app.phonebook.base.utils.PERMISSION_WRITE_CONTACTS
@@ -18,6 +22,25 @@ val Context.contactsDB: ContactsDao
     get() = ContactsDatabase.getInstance(applicationContext).ContactsDao()
 
 val Context.groupsDB: GroupsDao get() = ContactsDatabase.getInstance(applicationContext).GroupsDao()
+
+fun Context.getPhotoThumbnailSize(): Int {
+    val uri = ContactsContract.DisplayPhoto.CONTENT_MAX_DIMENSIONS_URI
+    val projection = arrayOf(ContactsContract.DisplayPhoto.THUMBNAIL_MAX_DIM)
+    var cursor: Cursor? = null
+    try {
+        cursor = contentResolver.query(uri, projection, null, null, null)
+        if (cursor?.moveToFirst() == true) {
+            return cursor.getIntValue(ContactsContract.DisplayPhoto.THUMBNAIL_MAX_DIM)
+        }
+    } catch (ignored: Exception) {
+        Log.e(APP_NAME, "getPhotoThumbnailSize: ${ignored.message}")
+    } finally {
+        cursor?.close()
+    }
+
+    return 0
+}
+
 
 fun Context.getEmptyContact(): Contact {
     val originalContactSource =
