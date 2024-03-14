02b73739b8fc4754264dcdfe5a076d973d4fcbed
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-03-14 09:13:22 +0700

    fast scroller

---
+++ app/src/main/java/com/app/phonebook/presentation/view/ItemIndicatorsUpdater.kt
@@ -0,0 +1,24 @@
+package com.app.phonebook.presentation.view
+
+import androidx.recyclerview.widget.RecyclerView
+
+open class ItemIndicatorsBuilder {
+    open fun buildItemIndicators(
+        recyclerView: RecyclerView,
+        getItemIndicator: (Int) -> FastScrollItemIndicator?,
+        showIndicator: ((FastScrollItemIndicator, Int, Int) -> Boolean)?
+    ): List<ItemIndicatorWithPosition> {
+        return (0 until recyclerView.adapter!!.itemCount)
+            .mapNotNull { position ->
+                getItemIndicator(position)?.let { ItemIndicatorWithPosition(it, position) }
+            }
+            .distinctBy(ItemIndicatorWithPosition::first)
+            .let { unfilteredIndicators ->
+                showIndicator?.let {
+                    unfilteredIndicators.filterIndexed { index, (indicator, _) ->
+                        it(indicator, index, unfilteredIndicators.size)
+                    }
+                } ?: unfilteredIndicators
+            }
+    }
+}
