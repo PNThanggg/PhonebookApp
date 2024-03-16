7518b5587417346c2babfc35137a5d58a3aa42af
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-03-16 12:48:16 +0700

    create::ViewPagerAdapter

---
+++ app/src/main/java/com/app/phonebook/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,53 @@
+package com.app.phonebook.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import com.app.phonebook.R
+import com.app.phonebook.base.extension.config
+import com.app.phonebook.base.utils.TAB_CALL_HISTORY
+import com.app.phonebook.base.utils.TAB_CONTACTS
+import com.app.phonebook.base.utils.TAB_FAVORITES
+import com.app.phonebook.base.utils.tabsList
+import com.app.phonebook.base.view.BaseActivity
+import com.app.phonebook.base.view.BaseViewPagerFragment
+
+class ViewPagerAdapter(val activity: BaseActivity<*>) : PagerAdapter() {
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val layout = getFragment(position)
+        val view = activity.layoutInflater.inflate(layout, container, false)
+        container.addView(view)
+
+        (view as BaseViewPagerFragment<*>).apply {
+            setupFragment(activity)
+        }
+
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, item: Any) {
+        container.removeView(item as View)
+    }
+
+    override fun getCount() = tabsList.filter { it and activity.config.showTabs != 0 }.size
+
+    override fun isViewFromObject(view: View, item: Any) = view == item
+
+    private fun getFragment(position: Int): Int {
+        val showTabs = activity.config.showTabs
+        val fragments = arrayListOf<Int>()
+        if (showTabs and TAB_CONTACTS > 0) {
+            fragments.add(R.layout.fragment_contacts)
+        }
+
+        if (showTabs and TAB_FAVORITES > 0) {
+            fragments.add(R.layout.fragment_favorites)
+        }
+
+        if (showTabs and TAB_CALL_HISTORY > 0) {
+            fragments.add(R.layout.fragment_recent)
+        }
+
+        return if (position < fragments.size) fragments[position] else fragments.last()
+    }
+}
