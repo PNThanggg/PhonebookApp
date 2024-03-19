d354609a0c09c8dc01d03f6077ebe78f8b91b029
PNThanggg <p.n.thang0101@gmail.com>
2024-03-20 02:30:30 +0700

    :create: constants

---
+++ app/src/main/java/com/app/phonebook/helpers/Constants.kt
@@ -0,0 +1,5 @@
+package com.app.phonebook.helpers
+
+private const val PATH = "com.app.phonebook.action."
+const val ACCEPT_CALL = PATH + "accept_call"
+const val DECLINE_CALL = PATH + "decline_call"
