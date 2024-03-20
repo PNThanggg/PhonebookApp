7816d5b6b23ce4484112af5c2abb376a5650177c
PNThanggg <p.n.thang0101@gmail.com>
2024-03-20 11:49:56 +0700

    :white_check_mark: Simple Call Screening Service

--- app/src/main/java/com/app/phonebook/services/SimpleCallScreeningService.kt
+++ app/src/main/java/com/app/phonebook/services/SimpleCallScreeningService.kt
@@ -7,13 +7,36 @@ import com.app.phonebook.base.extension.getMyContactsCursor
 import com.app.phonebook.helpers.SimpleContactsHelper
 
 class SimpleCallScreeningService : CallScreeningService() {
-
+    /**
+     * Determines whether an incoming call should be allowed, blocked, or screened based on configured preferences.
+     *
+     * This function is called whenever there's an incoming call. It uses the phone number from the call details
+     * to check against user preferences and the device's contacts database to decide if the call should be blocked
+     * or allowed through. The decision is based on two settings: whether to block unknown numbers (numbers not found
+     * in the contacts database) and whether to block calls from hidden or anonymous numbers.
+     *
+     * @param callDetails The details of the incoming call, including the call handle which contains the phone number.
+     *
+     * Operation:
+     * - If the incoming call number is known (not null) and the configuration is set to block unknown numbers,
+     *   it checks if the number exists in the device's contacts. If the number is not found, the call is considered
+     *   for blocking.
+     * - If the incoming call number is hidden (null) and the configuration is set to block hidden numbers,
+     *   the call is immediately considered for blocking.
+     * - All other calls not meeting the above criteria are allowed through.
+     *
+     * This function delegates the decision to `respondToCall`, which performs the actual operation based on the
+     * `isBlocked` flag determined by this function's logic.
+     */
     override fun onScreenCall(callDetails: Call.Details) {
         val number = callDetails.handle?.schemeSpecificPart
         when {
             number != null && baseConfig.blockUnknownNumbers -> {
                 val simpleContactsHelper = SimpleContactsHelper(this)
-                val privateCursor = getMyContactsCursor(favoritesOnly = false, withPhoneNumbersOnly = true)
+                val privateCursor = getMyContactsCursor(
+                    favoritesOnly = false,
+                    withPhoneNumbersOnly = true
+                )
                 simpleContactsHelper.exists(number, privateCursor) { exists ->
                     respondToCall(callDetails, isBlocked = !exists)
                 }
