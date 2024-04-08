7c2de2339f9c36e0ec169e04fb9f12f74adce1e2
PNThanggg <p.n.thang0101@gmail.com>
2024-04-09 02:07:30 +0700

    :create: create view contact activity

---
+++ app/src/main/java/com/app/phonebook/presentation/activities/ViewContactActivity.kt
@@ -0,0 +1,12 @@
+package com.app.phonebook.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.phonebook.R
+
+class ViewContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_contact)
+    }
+}
